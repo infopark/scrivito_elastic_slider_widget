9b53f7420acee28ec8a616849a09ed225c95728e
Karina <kt@jetaido.com>
2014-12-29 15:23:12 +0100

    only reloading carousel upon adding panel

--- app/assets/javascripts/scrivito_elastic_slider_widget/editing.js
+++ app/assets/javascripts/scrivito_elastic_slider_widget/editing.js
@@ -1,7 +1,8 @@
 $(".scrivito_elastic_slider_widget_add-new-panel").bind("click", function(e) {
   e.preventDefault();
-  $.get("/scrivito_elastic_slider_widget/add_new_panel?obj=" + scrivito.obj.current_page.id() + "&widget=" + $(this).attr("widgetid"), function(data) {
-    location.reload();
+  var widgetid = $(this).attr("widgetid");
+  $.get("/scrivito_elastic_slider_widget/add_new_panel?obj=" + scrivito.obj.current_page.id() + "&widget=" + widgetid, function(data) {
+    $("#carousel" + widgetid).trigger("scrivito_reload");
   });
 
 });
