7410abf02cc8ec283e7261ce646045d27a80fd7a
Karina <kt@jetaido.com>
2015-03-05 11:08:11 +0100

    fixing 'New Panel' for SDK 0.42.

--- app/assets/javascripts/scrivito_elastic_slider_widget/editing.js
+++ app/assets/javascripts/scrivito_elastic_slider_widget/editing.js
@@ -1,7 +1,9 @@
 $(".scrivito_elastic_slider_widget_add-new-panel").bind("click", function(e) {
   e.preventDefault();
+  var pageid = $(this).attr("pageid");
   var widgetid = $(this).attr("widgetid");
-  $.get("/scrivito_elastic_slider_widget/add_new_panel?obj=" + scrivito.obj.current_page.id() + "&widget=" + widgetid, function(data) {
+  $.get("/scrivito_elastic_slider_widget/add_new_panel?obj=" + pageid + "&widget=" + widgetid, function(data) {
+    // TBD: trigger("scrivito_reload") is deprecated but new method wasn't yet documented in March '15
     $("#carousel" + widgetid).trigger("scrivito_reload");
   });
 
